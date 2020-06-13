494ce84d90b541e9aea14679842bb5ac7d35f88a
Atishay <xatishayx@pop-os.localdomain>
2020-06-13 23:54:35 +0530

    added dashboard

---
+++ src/components/Content.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+
+class Content extends Component {
+  render() {
+    return (
+      <div className="next-steps my-5">
+    <h1 className="mb-4 text-center">Description   </h1>          
+
+      </div>
+    );
+  }
+}
+
+export default Content;
---
+++ src/views/Dashboard.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from "react";
+
+
+const Dashboard = () => (
+  <Fragment>
+            Dashboard come in.
+  </Fragment>
+);
+
+export default Dashboard;
