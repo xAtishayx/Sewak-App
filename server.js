74d782695fef1b0cf038c8e6f39da64830a5778c
blurry-x-face <rishabhshukla1999@gmail.com>
2020-06-27 13:00:41 +0530

    deploy script

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/public", "index.html"));
+  res.sendFile(path.join(__dirname, "./client/build", "index.html"));
 });
 
 app.use("/api/user", user);
